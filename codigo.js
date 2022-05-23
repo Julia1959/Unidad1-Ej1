1d64897534a4c3ed48602c7675ec4e7c7d8fffa7
julia <juliapateiro@gmail.com>
2022-05-22 21:28:44 -0300

    Unidad 1, Ej. 1

---
+++ codigo.js
@@ -0,0 +1,10 @@
+var dia = 10;
+var mes = 9;
+var año = 1959;
+var nfav = 8;
+
+
+console.log("El día de mi cumpleaños es", dia);
+console.log("El mes de mi cumpleaños en número es", mes);
+console.log("Mi año de nacimiento es", año);
+console.log("Mi número favorito es el", nfav);
